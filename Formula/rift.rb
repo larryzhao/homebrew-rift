730642d69114deaec1ead3e63f03f12d76027237
Larry Zhao <thehiddendepth@gmail.com>
2026-06-29 20:10:56 +0800

    rift 0.7.0

--- Formula/rift.rb
+++ Formula/rift.rb
@@ -1,17 +1,17 @@
 class Rift < Formula
   desc "Manage and run proxy connections from the command line"
   homepage "https://github.com/larryzhao/rift"
-  version "0.6.2"
+  version "0.7.0"
   license "GPL-3.0-or-later"
 
   on_macos do
     on_arm do
-      url "https://github.com/larryzhao/rift/releases/download/v0.6.2/rift-0.6.2-darwin-arm64.tar.gz"
-      sha256 "bb8e9b221625b0d659e9a8cb815a7527bdf7b9c6d3dab9a9dd2d60dae7366c7c"
+      url "https://github.com/larryzhao/rift/releases/download/v0.7.0/rift-0.7.0-darwin-arm64.tar.gz"
+      sha256 "535d29021bb367bf897475a57e7c4893c7d82500dce756db8d14fcc1deae4ad9"
     end
     on_intel do
-      url "https://github.com/larryzhao/rift/releases/download/v0.6.2/rift-0.6.2-darwin-amd64.tar.gz"
-      sha256 "341154fe1d8220e2fb7ec9a630254fb52bda6badfe61fc0cbe9de57a91d9fb66"
+      url "https://github.com/larryzhao/rift/releases/download/v0.7.0/rift-0.7.0-darwin-amd64.tar.gz"
+      sha256 "d4eac2a91a6c3d9fd9e9c3a6a4cc6f5e679d347d2a1e57405dc6d4ebb0716350"
     end
   end
 
